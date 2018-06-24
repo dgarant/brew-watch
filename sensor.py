dae7061afb96b3b39b2c9b6cd36e2fa642f9f4f3
Dan Garant <dangarant@gmail.com>
2018-06-24 23:53:20 +0000

    Adding freezer controller and temperature probe

--- sensor.py
+++ sensor.py
@@ -11,14 +11,14 @@ import time
 SENSOR_PIN=14
 POWERSWITCH_PIN=15
 
-# the temperature at which the heating pad will be activated
-TEMPERATURE_ON_THRESHOLD=71
-TEMPERATURE_OFF_THRESHOLD=73
+# freezer temperature thresholds for on/off
+TEMPERATURE_ON_THRESHOLD=68
+TEMPERATURE_OFF_THRESHOLD=64
 
 def read_probe_temp():
     sensor = W1ThermSensor()
     return sensor.get_temperature(W1ThermSensor.DEGREES_F)
-#print(read_probe_temp())
+probe_temp_f = read_probe_temp()
 
 # Try to grab a sensor reading.  Use the read_retry method which will retry up
 # to 15 times to get a sensor reading (waiting 2 seconds between each retry).
@@ -31,29 +31,30 @@ f_temperature_bb = temperature * 9/5. + 32
 # If this happens try again!
 if humidity is not None and temperature is not None:
 	print('Breadboard: Temp={0:0.1f}*F  Humidity={1:0.1f}%'.format(f_temperature_bb, humidity))
-	#print("Probe: Temp={0:0.1f}*F".format(f_temperature_probe))
+	print("Probe: Temp={0:0.1f}*F".format(probe_temp_f))
 else:
 	print 'Failed to get reading!'
 
+
 action = None
 pin_15_on = None
-if False: # disabling heating pad
+if True: # disabling power
     GPIO.setwarnings(False)
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(15, GPIO.OUT)
     with open("/sys/class/gpio/gpio15/value") as pin:
 	    pin_15_on = int(pin.read(1))
 
-    if f_temperature_bb >= TEMPERATURE_OFF_THRESHOLD and pin_15_on:
-	    print("Turning off heat")
+    if f_temperature_bb <= TEMPERATURE_OFF_THRESHOLD and pin_15_on:
+	    print("Turning off freezer")
 	    action = "off"
 	    GPIO.output(15, False)	
-    elif f_temperature_bb <= TEMPERATURE_ON_THRESHOLD and not pin_15_on:
-	    print("Turning on heat")
+    elif f_temperature_bb >= TEMPERATURE_ON_THRESHOLD and not pin_15_on:
+	    print("Turning on freezer")
 	    action = "on"
 	    GPIO.output(15, True)	
 
-if False: 
+if True: 
     tsl = TSL2561(0x39,"/dev/i2c-1")
     tsl.enable_autogain()
     tsl.set_time(0x00)
@@ -68,7 +69,9 @@ else:
     lux_median = None
 
 data={"temp" : temperature, 
-      "temp_scale" : "C", 
+      "temp_scale" : "F", 
+      "probe_temp" : probe_temp_f,
+      "probe_temp_scale" : "F",
       "action" : action,
       "is_heat_on" : pin_15_on,
       "humidity_pct" : humidity,
@@ -76,7 +79,7 @@ data={"temp" : temperature,
       "access_token" : os.environ["BW_ACCESS_TOKEN"]
 	}
 print(data)
-resp = requests.post("http://temp.garantanalytics.com/", json=data,
+resp = requests.post("http://brew-watch.garantanalytics.com/", json=data,
 		headers={"content-type" : "application/json"})
 print(resp)
 print(resp.text)
